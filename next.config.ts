481bce7c2f8aefe9ce3c15d297e680958da0d9a3
Cameron Campion <cameroncampion2018@gmail.com>
2025-07-01 23:00:09 +0000

    Went to list a plant with another account and encountered this: "Error:

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      { // Added for Google User Avatars
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
