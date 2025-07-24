ccd6aa75c48612862608382ef7364a5215fae6c0
Cameron Campion <cameroncampion2018@gmail.com>
2025-07-24 12:36:36 +0000

    We got this when signing in via google: "Error: An unexpected Turbopack

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -34,19 +34,14 @@ const signupSchema = z.object({
 
 type SignupFormValues = z.infer<typeof signupSchema>;
 
-function GoogleLogo(props: React.SVGProps<SVGSVGElement>) {
-    return (
-      <svg
-        role="img"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-      >
-        <title>Google</title>
-        <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.02 1.02-2.62 1.9-4.63 1.9-3.87 0-7-3.13-7-7s3.13-7 7-7c1.73 0 3.26.56 4.48 1.73l2.43-2.43C17.47 1.88 15.22 1 12.48 1 5.88 1 1 5.88 1 12.48s4.88 11.48 11.48 11.48c3.22 0 5.85-1.13 7.84-3.13 2.1-2.1 2.53-5.22 2.53-8.32 0-.6-.05-1.18-.15-1.73H12.48z" />
-      </svg>
-    );
-}
+const GoogleLogo = () => (
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
+      <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
+      <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
+      <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
+    </svg>
+);
 
 export default function SignupPage() {
   const router = useRouter();
@@ -224,8 +219,8 @@ export default function SignupPage() {
         </div>
       </div>
       <Button variant="outline" className="w-full" onClick={handleGoogleSignUp}>
-        <GoogleLogo className="mr-2 h-4 w-4" />
-        Sign up with Google
+        <GoogleLogo />
+        <span className="ml-2">Sign up with Google</span>
       </Button>
       <div className="mt-4 text-center text-sm">
         Already have an account?{" "}
