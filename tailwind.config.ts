d19f4641088517b682523103129a3a2362e4d63b
Cameron Campion <cameroncampion2018@gmail.com>
2025-08-04 18:35:57 +0000

    It now looks worse

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,16 +85,11 @@ export default {
   				to: {
   					height: '0'
   				}
-  			},
-            'wave': {
-                '0%, 100%': { transform: 'translateX(0)' },
-                '50%': { transform: 'translateX(-5%)' },
-            }
+  			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-            'wave': 'wave 8s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite',
   		}
   	}
   },
